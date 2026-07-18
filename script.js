5b7ae79eff86d86ca5a40765e6003155ba1eeff5
pierry-savio <pierrysavio.pi@gmail.com>
2026-07-18 11:29:51 -0300

    feat: mobile nav

---
+++ script.js
@@ -0,0 +1,20 @@
+// NAV MOBILE
+
+//Mobile footer
+const mobile_footer = document.getElementById("mobile_footer");
+//Close nav button
+const close_nav_mobile_button = document.getElementById("close_nav_mobile_button");
+//Open nav button
+const open_nav_mobile_button = document.getElementById("open_nav_mobile_button");
+
+//CLOSE NAV
+close_nav_mobile_button.addEventListener("click", () =>{
+    mobile_footer.classList.add("closed");
+    open_nav_mobile_button.classList.remove('closed');
+});
+
+//OPEN NAV
+open_nav_mobile_button.addEventListener("click", () =>{
+    mobile_footer.classList.remove("closed");
+    open_nav_mobile_button.classList.add('closed');
+});
