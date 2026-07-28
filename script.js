bef662ed159e41d181e07182cfe05d3f238ea2ce
pizin brabin gameplays <pierrysavio.pi@gmail.com>
2026-07-28 11:13:20 -0300

    feat: carousel js - first implementation

--- script.js
+++ script.js
@@ -26,3 +26,80 @@ const nav_pc = document.getElementById("nav_pc");
 close_nav_pc_button.addEventListener("click", () =>{
     nav_pc.classList.toggle("closed");
 })
+
+/* --- PROJECTS --- */
+
+//Arrows
+const project_arrow_right = document.getElementById("project_arrow_right");
+const project_arrow_left = document.getElementById("project_arrow_left");
+
+//Cards
+const card1 = document.getElementById("project1");
+const card2 = document.getElementById("project2");
+const card3 = document.getElementById("project3");
+
+//Project index
+let currentProject = 1;
+
+project_arrow_right.addEventListener("click", () =>{
+    
+    if (currentProject < 3) {
+        currentProject+=1;
+    }
+    else{
+        currentProject = 1;
+    }
+
+    switch (currentProject){
+        case 1:
+            card3.classList.add("closed-left");
+            card1.classList.remove("closed-right");
+            card1.classList.remove("closed-left");
+        break;
+
+        case 2:
+            card1.classList.add("closed-left");
+            card2.classList.remove("closed-right");
+            card2.classList.remove("closed-left");
+        break;
+
+        case 3:
+            card2.classList.add("closed-left");
+            card3.classList.remove("closed-right");
+            card3.classList.remove("closed-left");
+        break;
+    }
+
+    console.log(currentProject);
+});
+
+project_arrow_left.addEventListener("click", () =>{
+    if (currentProject > 1) {
+        currentProject-=1;
+    }
+    else{
+        currentProject = 3;
+    }
+
+    switch (currentProject){
+        case 1:
+            card2.classList.add("closed-right");
+            card1.classList.remove("closed-left");
+            card1.classList.remove("closed-right");
+        break;
+
+        case 2:
+            card3.classList.add("closed-right");
+            card2.classList.remove("closed-left");
+            card2.classList.remove("closed-right");
+        break;
+
+        case 3:
+            card1.classList.add("closed-right");
+            card3.classList.remove("closed-left");
+            card3.classList.remove("closed-right");
+        break;
+    }
+
+    console.log(currentProject);
+});
