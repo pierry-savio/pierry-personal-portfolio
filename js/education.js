9fde41bc8538c22e35342938015ecbb9c438ee90
pierry <pierrysavio.pi@gmail.com>
2026-08-17 11:49:22 -0300

    feat: education

--- js/education.js
+++ js/education.js
@@ -1,5 +1,17 @@
-const course = document.getElementById('course');
+const course_senai = document.getElementById('course_senai');
+const course_fatec = document.getElementById('course_fatec');
+const course_marlene = document.getElementById('course_marlene');
 
-course.addEventListener('click', () => {
-    course.classList.toggle('closed');
+course_senai.addEventListener('click', () => {
+    course_senai.classList.toggle('closed');
 });
+
+course_fatec.addEventListener('click', () => {
+    course_fatec.classList.toggle('closed');
+});
+
+course_marlene.addEventListener('click', () => {
+    course_marlene.classList.toggle('closed');
+});
+
+
